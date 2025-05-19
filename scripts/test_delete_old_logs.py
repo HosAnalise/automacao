106ccb0fc83afd415f8e0fc3ee0b90441d73fb1e
GabrKin <gabriel.kin@hos.com.br>
2025-05-19 11:09:17 -0300

    fix: alterações feitas no script para rodar corretamente

--- scripts/test_delete_old_logs.py
+++ scripts/test_delete_old_logs.py
@@ -1,10 +1,8 @@
-from pickle import TRUE
-from unittest import result
 from classes.utils.LogManager import LogManager
 
 log = LogManager()
 
-# log.delete_logs_older_than(0)
+log.delete_logs_older_than(0)
 
-data = log.get_logs()
-print(data)
+# data = log.get_logs()
+# print(data)
