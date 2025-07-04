787904ba67ca8896a8995c10939e900b9b04a1b0
HosAnalise <hos_analise@hos.com.br>
2025-07-04 11:06:03 -0300

    novo

--- scripts/rank_logs.py
+++ scripts/rank_logs.py
@@ -4,7 +4,8 @@ from classes.utils.LogManager import LogManager
 log_manager =  LogManager()
 
 
+def test_error():
 
 
 
-print(log_manager.analisar_erros())
+    print(log_manager.analisar_erros())
