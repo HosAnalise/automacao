862185cef120ea3beff2bc115f853d9a5f8f038c
HosAnalise <hos_analise@hos.com.br>
2025-07-04 10:29:44 -0300

    teste

--- scripts/build.py
+++ scripts/build.py
@@ -64,7 +64,7 @@ def write_docker_compose(config, filename='docker-compose.yml'):
         print(f" Erro ao escrever o arquivo YAML: {e}")
 
 def main(marker_source='pytest.ini', output_file='docker-compose.yml'):
-    """
+    """ 
     Cria o arquivo docker-compose.yml com base nos marcadores do pytest.
     """
     print(" Iniciando a criação do docker-compose dinâmico...")
