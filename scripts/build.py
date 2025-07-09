634e3de2bd00ef0232cd53767a05311b3673b573
HosAnalise <hos_analise@hos.com.br>
2025-07-09 09:16:52 -0300

    chore: update build script to remove unused environment variable

--- scripts/build.py
+++ scripts/build.py
@@ -46,7 +46,7 @@ def get_base_config(marker: str):
                 'image': f'hosanalise/{safe_service_name}:latest',
                 
             },
-            'commands': ['pytest', f'--markers={marker}'],               
+            'commands': [['pytest', '-m',f'{marker}']],               
             
         }
     }
